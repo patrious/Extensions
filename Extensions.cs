969c9586b1f7c90ff24909aaf37a3b5e22376df7
patrious <mikolajczak.patrick@gmail.com>
2015-02-21 18:56:43 -0500

    Init

---
+++ Extensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static byte[] GetBytes(this string str)
+        {
+            var bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public static string GetString(this byte[] bytes)
+        {
+            var chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        public static void CallWithTimeout(this Action action, int timeoutMilliseconds)
+        {
+            Thread threadToKill = null;
+            Action wrappedAction = () =>
+            {
+                threadToKill = Thread.CurrentThread;
+                action();
+            };
+
+            var result = wrappedAction.BeginInvoke(null, null);
+            if (result.AsyncWaitHandle.WaitOne(timeoutMilliseconds))
+            {
+                wrappedAction.EndInvoke(result);
+            }
+            else
+            {
+                threadToKill.Abort();
+                Console.WriteLine("Failed to complete task in time");
+            }
+        }
+
+        public static List<int> PickUniqueRandomNumbers(this Random rand, int numberOfItems, int max = 1, int min = 0)
+        {
+            Debug.Assert(numberOfItems < max - min);
+
+            var returnNumbers = new List<int>();
+            while (returnNumbers.Count < numberOfItems)
+            {
+                returnNumbers.Add(rand.Next(min, max));
+            }
+            return returnNumbers;
+        }
+
+        public static T DeepClone<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+    }
+}
